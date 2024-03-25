2cacf8baf5d05e4d64f913f19bf76d91318d29a3
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-25 04:54:45 +0100

    first commit

---
+++ mydb.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+#connect to mysql
+dataBase = mysql.connector.connect(
+  host= "localhost",
+  user= "root",
+  passwd= os.environ.get('MYSQL_PASSWORD'),
+  auth_plugin='mysql_native_password'
+
+)
+
+#prepare a cursor object
+cursorObject = dataBase.cursor()
+
+#create a db
+cursorObject.execute("CREATE DATABASE dcrm")
+
+print("database done!")
