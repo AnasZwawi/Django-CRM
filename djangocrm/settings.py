7fa77b66b568a276829beaa5445a17effdb4c1b0
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-27 14:20:39 +0100

    production ready

--- djangocrm/settings.py
+++ djangocrm/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-_qh=f@jh2so4usu5=kt%tdsg+$-vw$u5z39h(&ra$l#fsy4!iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
