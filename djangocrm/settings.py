1df1f420053d4acb2b26d0b4bdfaa210935804c5
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-27 10:45:45 +0100

    last commit

--- djangocrm/settings.py
+++ djangocrm/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-_qh=f@jh2so4usu5=kt%tdsg+$-vw$u5z39h(&ra$l#fsy4!iy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -113,6 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
