f163cacaf8cb81c178a7138c6b2a6c9d911bfdbc
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-27 06:45:42 +0100

    view and delete record

--- website/views.py
+++ website/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
-
+from .models import Record
 
 def home(request):
+  records = Record.objects.all()
   #check to see if logging in
   if request.method == 'POST':
     username = request.POST['username']
@@ -20,11 +21,18 @@ def home(request):
       return redirect('home')
  
   else:
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', {'records' : records})
+
+
+
+
 
 def logout_user(request):
   logout(request)
   return redirect('home')
+
+
+
 
 def register_user(request):
   if request.method == 'POST':
@@ -43,3 +51,27 @@ def register_user(request):
     return render(request, 'register.html', {'form': form})
   
   return render(request, 'register.html', {'form': form})
+
+
+
+
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
+
+
+
+
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record Deleted Successfully...")
+		return redirect('home')
+	else:
+		messages.success(request, "You Must Be Logged In To Do That...")
+		return redirect('home')
